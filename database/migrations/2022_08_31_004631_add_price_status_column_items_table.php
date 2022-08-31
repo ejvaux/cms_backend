7086981656e950fb6516667ca93ba7899a656bd8
EJ Mati <ejvaux_05126@yahoo.com>
2022-08-31 08:53:13 +0800

    Added columns price and status in items table

---
+++ database/migrations/2022_08_31_004631_add_price_status_column_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPriceStatusColumnItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('items', function (Blueprint $table) {
+            $table->decimal('price', 9, 3)->nullable()->after('max');
+            $table->string('status')->nullable()->after('site_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('items', function (Blueprint $table) {
+            $table->dropColumn(['price', 'status']);
+        });
+    }
+}
